7cb58ffbca59ff3f84cee0ec62afc6f551ee8d9c
Lucas Seiti Taba <seiti.taba@gmail.com>
2018-06-14 17:28:57 -0400

    Configuração de segurança.

--- root/web/tcc/openLock.php
+++ root/web/tcc/openLock.php
@@ -1,18 +1,23 @@
 <?php
-	$socket = socket_create(AF_UNIX,SOCK_STREAM,0);
-	// $command = "chown root:root /temp/programManager.socket";
-	// exec($command);
-	$addr = "/tmp/programManager.socket";
-	if($socket == false){
-		echo "socket_create() failed".socket_strerror(socket_last_error());
+
+	include('session.php');
+
+	if($_SERVER["REQUEST_METHOD"] == "GET"){
+		$socket = socket_create(AF_UNIX,SOCK_STREAM,0);
+		// $command = "chown root:root /temp/programManager.socket";
+		// exec($command);
+		$addr = "/tmp/programManager.socket";
+		if($socket == false){
+			echo "socket_create() failed".socket_strerror(socket_last_error());
+		}
+		$result = socket_connect($socket,$addr,0);
+		if($result == false){
+			echo "socket_connect() failed:".socket_strerror(socket_last_error());
+		}
+		$msg = "04";
+		socket_write($socket, $msg, strlen($msg));
+		echo $msg;
+		socket_close($socket);
 	}
-	$result = socket_connect($socket,$addr,0);
-	if($result == false){
-		echo "socket_connect() failed:".socket_strerror(socket_last_error());
-	}
-	$msg = "04";
-	socket_write($socket, $msg, strlen($msg));
-	echo $msg;
-	socket_close($socket);
 
 ?>
